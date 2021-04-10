c8a47dd4daf63756e9f12f037d397ece94b714ab
kittyandrew <kitty@maudrew.dev>
2021-04-11 00:12:57 +0300

    remove outdated comment

--- scripts/fast_compute/src/lib.rs
+++ scripts/fast_compute/src/lib.rs
@@ -13,8 +13,6 @@ py_module_initializer!(fast_compute, |py, m| {
 
 
 fn compute(_py: Python, modulus: usize, k: usize) -> PyResult<(usize, HashMap<usize, usize>)> {
-    // Computing amount of combinations we will have
-    // to initate array (vector) of needed length.
     let mut set_size: usize = 0;
     let mut lengths: HashMap<usize, usize> = HashMap::new();
     for subset in (0..modulus).combinations(k) {
